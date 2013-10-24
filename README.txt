fc6467685ed9fc93514086834c2307c674cf4001
Aaron Ash <aaron.ash@gmail.com>
2013-10-24 20:01:19 +1000

    Rustructured for pip package

---
+++ README.txt
@@ -0,0 +1,6 @@
+========
+Py Trade
+========
+
+Py Trade is a trading systems tester written in python.  It uses yahoo finance data and is aimed at the ASX companies for now.
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='PyTrade',
+    version='0.1.0',
+    author='Aaron Ash',
+    author_email='aaron.ash+pytrade@gmail.com',
+    packages=['pytrade'],
+    scripts=['bin/moving-average-system.py'],
+    url='http://github.com/AshyIsMe/PyTrade',
+    license='LICENSE.txt',
+    description='Python trading systems',
+    long_description=open('README.txt').read(),
+    install_requires=[
+      "requests",
+    ],
+)
